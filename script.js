8fcbd43ffe22453f6ecd705c8ce7e026beaa39fa
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2025-07-03 15:13:14 -0400

    add portfolio projects

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const tablinks = document.querySelectorAll(".tab-links");
 const tabcontents = document.querySelectorAll(".tab-contents");
+const sidemenu = document.getElementById("sidemenu");
+const openIcon = document.querySelector(".nav-expand");
+const closeIcon = document.querySelector(".nav-close");
+const toggleButtons = document.querySelectorAll(".toggle-button");
 
 function opentab(event, tabname) {
   for (let i = 0; i < tablinks.length; i++) {
@@ -11,8 +15,6 @@ function opentab(event, tabname) {
   }
 }
 
-const sidemenu = document.getElementById("sidemenu");
-
 function openmenu() {
   sidemenu.style.right = 0;
 }
@@ -20,3 +22,39 @@ function openmenu() {
 function closemenu() {
   sidemenu.style.right = "-100%";
 }
+
+openIcon.addEventListener("click", (event) => {
+  openmenu();
+});
+
+closeIcon.addEventListener("click", (event) => {
+  closemenu();
+});
+
+document.addEventListener("click", function (event) {
+  console.log(event.target);
+  const isClickInside =
+    sidemenu.contains(event.target) || openIcon.contains(event.target);
+
+  if (!isClickInside) {
+    closemenu();
+  }
+});
+
+toggleButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const button = e.currentTarget;
+
+    // Get the ID of the controlled element
+    const listId = button.getAttribute("aria-controls");
+    const detailsList = document.getElementById(listId);
+
+    const isExpanded = button.getAttribute("aria-expanded") === "true";
+
+    // Toggle hidden class
+    detailsList.classList.toggle("hidden");
+
+    // Update aria-expanded attribute
+    button.setAttribute("aria-expanded", !isExpanded);
+  });
+});
